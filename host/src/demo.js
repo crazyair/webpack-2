05ab7783ea87ef4cf2f5b4aded0e9f1c4d44f88a
黄建峰 <645381995@qq.com>
2021-03-17 12:31:55 +0800

    bug

--- host/src/demo.js
+++ host/src/demo.js
@@ -1,10 +1,25 @@
+import { Modal } from "antd";
 import React from "react";
+import { Form } from "@ant-design/compatible";
 import { Tag } from "yforms-provider";
 
 const App = () => {
   return (
     <>
       <Tag>host demo</Tag>
+      <a
+        onClick={() => {
+          Modal.confirm({
+            content: (
+              <>
+                <Form.Item>123</Form.Item>
+              </>
+            ),
+          });
+        }}
+      >
+        demo
+      </a>
     </>
   );
 };
--- remote/src/demo.js
+++ remote/src/demo.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Modal } from "antd";
 import { Tag } from "yforms-provider";
+import { Form } from "@ant-design/compatible";
 
 const App = () => {
   return (
     <>
       <Tag>remote demo</Tag>
+      <a
+        onClick={() => {
+          Modal.confirm({
+            content: (
+              <>
+                <Form.Item>123</Form.Item>
+              </>
+            ),
+          });
+        }}
+      >
+        demo
+      </a>
     </>
   );
 };
